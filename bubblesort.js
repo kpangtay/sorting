c00f3157eddba8e038f9a134f0c113e5400f98dc
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-04-23 14:03:32 -0400

    finished nested loop sorting

--- bubblesort.js
+++ bubblesort.js
@@ -1,10 +1,29 @@
 function bubbleSort(array){
   //take single and multiple items
-  return swap(array);
+  console.log(array);
+  if (!array.length){
+    return [];
+  }else{
+    for (let i = 0; i < array.length-1; i++){
+      for(let j = 0; j < array.length-1-i; j++){
+        if(compare(array[j], array[j+1]) === true){
+          let first = array[j];
+          array[j] = array[j+1];
+          array[j+1] = first;
+        }
+      }
+    }
+  }
+  console.log(array);
+  return array;
 }
 // bubbleSort.swap = function(array){
 //   return array;
 // }
-function swap(array){
-  return array;
+function compare(a, b){
+  if (a < b){
+    return false;
+  }else{
+    return true;
+  }
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -5,6 +5,8 @@ describe('Bubble Sort', function(){
 
   let testArr = [1, 3, 4, 2];
   let testSorted = [1, 2, 3, 4];
+  let test2 = [1,10,9,8,2,7,6,5,4,3];
+  let test2Sorted = [1,2,3,4,5,6,7,8,9,10];
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
@@ -17,12 +19,15 @@ describe('Bubble Sort', function(){
   it('properly sorts our test array', function(){
     expect(bubbleSort(testArr) ).toEqual(testSorted);
   });
+  it('properly sorts our 2nd test array', function(){
+    expect(bubbleSort(test2) ).toEqual(test2Sorted);
+  });
 
-  it('uses bubble sort the expected number of times', function(){
-    spyOn(window, 'swap').and.callThrough();
-    bubbleSort(testArr);
-    //expect(bubbleSort(testArr)).toEqual(testSorted);
-    expect(bubbleSort.swap.calls.count()).toEqual(1)
-  })
+  // it('uses bubble sort the expected number of times', function(){
+  //   spyOn(window, 'swap').and.callThrough();
+  //   bubbleSort(testArr);
+  //   //expect(bubbleSort(testArr)).toEqual(testSorted);
+  //   expect(bubbleSort.swap.calls.count()).toEqual(1)
+  // })
 });
 
