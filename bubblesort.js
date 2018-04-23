ff81361f6db9368864cb807cb4eae5b06cfc514c
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-04-23 12:26:35 -0400

    times up

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,10 @@
 function bubbleSort(array){
   //take single and multiple items
+  return swap(array);
+}
+// bubbleSort.swap = function(array){
+//   return array;
+// }
+function swap(array){
   return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,10 @@
 describe('Bubble Sort', function(){
+  // beforeAll(function(){
+
+  // });
+
+  let testArr = [1, 3, 4, 2];
+  let testSorted = [1, 2, 3, 4];
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
@@ -8,5 +14,15 @@ describe('Bubble Sort', function(){
   it('handles multiple', function(){
     expect(bubbleSort([1,2,3]) ).toEqual( [1,2,3]);
   });
+  it('properly sorts our test array', function(){
+    expect(bubbleSort(testArr) ).toEqual(testSorted);
+  });
+
+  it('uses bubble sort the expected number of times', function(){
+    spyOn(window, 'swap').and.callThrough();
+    bubbleSort(testArr);
+    //expect(bubbleSort(testArr)).toEqual(testSorted);
+    expect(bubbleSort.swap.calls.count()).toEqual(1)
+  })
 });
 
